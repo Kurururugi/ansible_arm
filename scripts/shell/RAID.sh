3b7e64c90485ba1161324358f674f81882f4f748
KhodunovVI <hodunov.v@semgroup.ru>
2026-03-23 09:37:18 +0300

    add new RAID.sh

--- scripts/shell/RAID.sh
+++ scripts/shell/RAID.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
 disk_state=`megacli -LDInfo -LALL -aALL | grep State`
-if disk_state=='State               : Optimal'
+if [ "$disk_state" == 'State               : Optimal' ]
 then
-   echo 1 > /usr/local/Project/raid_state
+        echo 1 > /usr/local/Project/raid_state
 else
-   echo 0 > /usr/local/Project/raid_state
+        echo 0 > /usr/local/Project/raid_state
 fi
 
+time_state=`chronyc tracking | grep Leap`
+
+if [ -z "$time_state" ]
+then
+	time_state="err"
+fi
+
+if [ "$time_state" == 'Leap status     : Normal' ]
+then
+        echo 1 > /usr/local/Project/time_state
+else
+        echo 0 > /usr/local/Project/time_state
+fi
 exit 0
