3a2d11fd1c8a1d856f52798f912265aea247abd9
KhodunovVI <hodunov.v@semgroup.ru>
2026-04-15 16:57:16 +0300

    rewrite grub.pu

--- scripts/python/grub.py
+++ scripts/python/grub.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import os
+import re
 
 if not os.path.isfile('/etc/default/grub.bak'):
     os.system('cp /etc/default/grub /etc/default/grub.bak')
@@ -12,11 +13,28 @@ with open("/etc/default/grub", "w") as f:
         if 'GRUB_TIMEOUT' in line.strip():
             line = 'GRUB_TIMEOUT=0\n'
             f.write(line)
-        elif 'GRUB_CMDLINE_LINUX_DEFAULT=' in line.strip():
-            line = 'GRUB_CMDLINE_LINUX_DEFAULT="splash quiet parsec.mac=0 net.ifnames=0 ipv6.disable=1 audit=1"\n'
+        elif 'GRUB_CMDLINE_LINUX_DEFAULT' in line:
+            if 'ipv6.disable' in line:
+                line = re.sub(r'ipv6\.disable=[^\s"]+', 'ipv6.disable=1', line)
+            else:
+                if line.rstrip().endswith('"'):
+                    line = line[:-2] + ' ipv6.disable=1"\n'
+                else:
+                    line = line.rstrip() + ' ipv6.disable=1\n'
+
+            if 'audit' in line:
+                line = re.sub(r'audit=[^\s"]+', 'audit=1', line)
+            else:
+                if line.rstrip().endswith('"'):
+                    line = line[:-2] + ' audit=1"\n'
+                else:
+                    line = line.rstrip() + ' audit=1\n'
+
             f.write(line)
         else:
             f.write(line)
+
+
 
 os.system('''echo '\nGRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX init_on_alloc=1"'                              >> /etc/default/grub''')
 os.system('''echo 'GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX slab_nomerge"'                                   >> /etc/default/grub''')
@@ -25,6 +43,6 @@ os.system('''echo 'GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX randomize_kstack_offs
 os.system('''echo 'GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX mitigations=auto,nosmt"'                         >> /etc/default/grub''')
 os.system('''echo 'GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX vsyscall=none"'                                  >> /etc/default/grub''')
 os.system('''echo 'GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX debugfs=no-mount"'                               >> /etc/default/grub''')
-os.system('''echo 'GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX debugfs=tsx=off"'                                >> /etc/default/grub''')
+os.system('''echo 'GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX tsx=off"'                                >> /etc/default/grub''')
 
 os.system('update-grub')
